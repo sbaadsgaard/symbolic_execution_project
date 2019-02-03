01452fa6b0cb2a87056835bf756f7075186930eb
sbaadsgaard <sbaadsgaard@live.dk>
2019-02-03 16:39:12 +0100

    Initial build of interpreter. missing while statement

--- implementation/src/main/scala/Interpreter.scala
+++ implementation/src/main/scala/Interpreter.scala
@@ -1,3 +1,143 @@
-object Interpreter {
+import com.sun.net.httpserver.Authenticator.Failure
 
+import scala.collection.mutable
+import scala.util.Failure
+
+/**
+  * Simple interpreter for the language SImPL. We simply descent the AST.
+  */
+object Interpreter extends App {
+
+  /** ***********************************************************************/
+  /**
+    * We define the language here
+    */
+  //TODO perhaps try to split op binary operators.
+  sealed trait Expression
+
+  case class Integer(x: Int) extends Expression
+
+  case class Variable(name: String) extends Expression
+
+  case class PlusOp(e1: Expression, e2: Expression) extends Expression
+
+  case class MinusOp(e1: Expression, e2: Expression) extends Expression
+
+  case class MultOp(e1: Expression, e2: Expression) extends Expression
+
+  case class DivOp(e1: Expression, e2: Expression) extends Expression
+
+  case class GtOp(e1: Expression, e2: Expression) extends Expression
+
+  case class EqOp(e1: Expression, e2: Expression) extends Expression
+
+
+  sealed trait Statement
+
+  case class ExpStm(e: Expression) extends Statement
+
+  case class AssignStm(v: Variable, e: Expression) extends Statement
+
+  case class SeqStm(s1: Statement, s2: Statement) extends Statement
+
+  case class IfStm(cond: Expression, thenStm: Statement, elseStm: Statement) extends Statement
+
+  case class WhileStm(cond: Expression, doStm: Statement) extends Statement
+
+
+  /** ***********************************************************************/
+
+  case class EnvEntry(name: String, value: Int)
+
+  /**
+    * Entry method for interpreting a program. A program is simply a statement.
+    *
+    * @param stm statement representing the program
+    * @return the result of running the program.
+    */
+  def interp(stm: Statement): Int = {
+
+    /**
+      * One of two workhorses in the interpretation. used to recursively interpret Statements. calls interpExp whenever
+      * an expression must be interpreted.
+      *
+      * @param stm the statement to be interpreted
+      * @param env the current version of the environment; A list entries which binds a value to a name.
+      * @return the resulting value of the statement
+      */
+    def interpStm(stm: Statement, env: List[EnvEntry]): (Int, List[EnvEntry]) =
+      stm match {
+        case ExpStm(e) => {
+          val v = interpExp(e, env)
+          (v, env)
+        }
+        case AssignStm(Variable(name), e) => {
+          val v = interpExp(e, env)
+          val newEnv = EnvEntry(name, v) :: env
+          (v, newEnv)
+        }
+        case SeqStm(s1, s2) => {
+          val (_, env1) = interpStm(s1, env)
+          interpStm(s2, env1)
+        }
+        case IfStm(cond, thenStm, elseStm) => {
+          val v = interpExp(cond, env)
+          if (v != 0) interpStm(thenStm, env) else interpStm(elseStm, env)
+        }
+        case WhileStm(cond, doStm) => throw new NotImplementedError("coming soon!")
+      }
+
+    /**
+      * Second workhorse in the interpretation. Used to recursively interpret an expression.
+      *
+      * @param e   The expression to be interpreted.
+      * @param env The current version of the environment; A list of entries which binds a value to a name.
+      * @return the value of the expression.
+      */
+    def interpExp(e: Expression, env: List[EnvEntry]): Int =
+      e match {
+        case Integer(x) => x
+        case Variable(name) => {
+          val res = env.find(entry => entry.name == name)
+          res match {
+            case Some(entry) => entry.value
+            case None => throw new NoSuchElementException(s"can't find variable with name  ${name}")
+          }
+        }
+        case PlusOp(e1, e2) => interpExp(e1, env) + interpExp(e2, env)
+        case MinusOp(e1, e2) => interpExp(e1, env) - interpExp(e2, env)
+        case MultOp(e1, e2) => interpExp(e1, env) * interpExp(e2, env)
+        case DivOp(e1, e2) => interpExp(e1, env) / interpExp(e2, env)
+        case GtOp(e1, e2) => if (interpExp(e1, env) > interpExp(e2, env)) 1 else 0
+        case EqOp(e1, e2) => if (interpExp(e1, env) == interpExp(e2, env)) 1 else 0
+      }
+
+    // TODO: perhaps find a better way to model the environment. Currently we simply pass around lists of entries.
+    //where the first occurence of a name dictates the newest value.
+    val env: List[EnvEntry] = List.empty[EnvEntry]
+    val (res, _) = interpStm(stm, env)
+    res
+  }
+
+  // 2 + 2
+  val testProg1 = ExpStm(PlusOp(Integer(2), Integer(10)))
+
+  // If 2 > 3 then 1 else 2
+  val testProg2 = IfStm(GtOp(Integer(2), Integer(3)), ExpStm(Integer(1)), ExpStm(Integer(2)))
+
+  // a = 11
+  // a
+  val testProg3 = SeqStm(AssignStm(Variable("a"), Integer(11)), ExpStm(Variable("a")))
+
+  // b = 3
+  // if b > 2 then b = 2 else b
+  val testProg4 = SeqStm(
+    AssignStm(Variable("b"), Integer(3)),
+    IfStm(GtOp(Variable("b"), Integer(2)),
+      AssignStm(Variable("b"), Integer(2)),
+      ExpStm(Variable("b"))))
+
+  println(interp(testProg4))
 }
+
+
