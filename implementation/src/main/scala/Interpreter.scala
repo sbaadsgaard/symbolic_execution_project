653f71ded777fab2f28e9f6e40f16288ca748131
sbaadsgaard <sbaadsgaard@live.dk>
2019-02-15 12:44:27 +0100

    Add a driver class to contain testing. removed test code from Interpreter.

--- implementation/src/main/scala/Interpreter.scala
+++ implementation/src/main/scala/Interpreter.scala
@@ -14,7 +14,7 @@ import scala.collection.mutable
 /**
   * Simple interpreter for the language SImPL. We simply descent the AST.
   */
-object Interpreter extends App {
+object Interpreter {
 
   /** ***********************************************************************/
   /**
@@ -128,63 +128,6 @@ object Interpreter extends App {
     val venv: mutable.HashMap[Var, IntValue] = mutable.HashMap()
     interpStm(p.stm, venv)
   }
-
-  /*
-   * Func to build a Prog from a statement. takes a stm, and returns a prog with a map from id main to func main with
-   * the given stm as body
-   */
-
-  /*
-   * fun fib(n) = if (2 > n) n else fib(n-1) + fib(n-2)
-   * fib(12)
-   */
-
-  val testProg = Prog(mutable.HashMap("fib" -> FDecl("fib", List(Var("n")),
-    IfStm(
-      BExp.BinExp(Integer(2), Var("n"), GtOp()),
-      ExpStm(Var("n")),
-      ExpStm(
-        AExp.BinExp(
-          CallExp("fib", List(AExp.BinExp(Var("n"), Integer(1), Sub()))),
-          CallExp("fib", List(AExp.BinExp(Var("n"), Integer(2), Sub()))),
-          Plus()
-        )
-      )))),
-    ExpStm(CallExp("fib", List(Integer(12)))))
-
-
-  /*
-   * Test to ensure that locally defined variables does not escape function scope
-   */
-
-  val testProg1 = Prog(mutable.HashMap("test" -> FDecl("test", List(Var("a")),
-    ExpStm(AExp.BinExp(Var("a"), Integer(2), Mul())))),
-    CompStm(
-      ExpStm(CallExp("test", List(Integer(2)))),
-      ExpStm(Var("a"))
-    )
-  )
-
-  /*
-   * Test to ensure that mutations of global variables only exists in the function scope
-   */
-
-
-  /*
-   * fun pow(a, b) {
-   *  res = 1
-   *  i = 0
-   *  while (b > i) do
-   *    res = res*a
-   *    i = 1 + 1
-   *  res
-   * }
-   *
-   * pow(2,3)
-   */
-
-  val res = interpProg(testProg)
-  println(s"result was: ${res.toString}")
 }
 
 
