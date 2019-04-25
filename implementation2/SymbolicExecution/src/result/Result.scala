828bb65088cabb5565feef7ad84bbc21d9e397cb
sbaadsgaard <sbaadsgaard@live.dk>
2019-04-25 10:55:39 +0200

    added sequence and traverse, to evaluate over lists

--- implementation2/SymbolicExecution/src/result/Result.scala
+++ implementation2/SymbolicExecution/src/result/Result.scala
@@ -1,5 +1,4 @@
 package result
-// read up on type system of scala
 trait  Result[+V, +E] {
   def map[T](f: V => T): Result[T, E] = this match {
     case Ok(v) => Ok(f(v))
@@ -10,7 +9,21 @@ trait  Result[+V, +E] {
     case Ok(v) => f(v)
     case Error(e) => Error(e)
   }
+
+
 }
+
 case class Ok[V](v: V) extends Result[V, Nothing]
 case class Error[E](e: E) extends Result[Nothing, E]
 
+object Result {
+  def sequence[V, E](vs: List[Result[V,E]]): Result[List[V], E] = vs match {
+    case Nil => Ok(Nil)
+    case hd::tl => hd.flatMap( v => sequence(tl).map(v :: _))
+  }
+
+  def traverse[V, W, E](vs: List[V])(f: V => Result[W, E]): Result[List[W], E] = vs match {
+    case Nil => Ok(Nil)
+    case hd::tl => f(hd).flatMap( w => traverse(tl)(f).map(w :: _))
+  }
+}
